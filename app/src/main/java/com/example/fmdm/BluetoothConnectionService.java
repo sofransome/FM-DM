9238386f49e7a486b9bdf328c90bac7bd3134673
Francis Juridico <sofransome@gmail.com>
2021-04-17 10:32:25 +0800

    Thesis(FM&DM)v1

--- app/src/main/java/com/example/fmdm/BluetoothConnectionService.java
+++ app/src/main/java/com/example/fmdm/BluetoothConnectionService.java
@@ -363,7 +363,7 @@ public class BluetoothConnectionService {
                         System.out.println("contact7: " + contact7);
 
                         //a
-                        if((thumb>=1.00 && thumb<=1.00) && (index>=1.00 && index<=1.00) && (middle>=1.00 && middle<=1.00) && (ring>=1.00 && ring<=1.00) && (pinky>=1.00  && pinky<=1.00)){
+                        if((thumb>=2.86 && thumb<=2.98) && (index>=2.74 && index<=2.95) && (middle>=2.82 && middle<=2.95) && (ring>=2.80 && ring<=3.10) && (pinky>=3.00  && pinky<=3.40) && (contact1 == 1.00)){
                             Message message = Message.obtain();
 
                             Bundle bundle = new Bundle();
@@ -371,6 +371,232 @@ public class BluetoothConnectionService {
                             message.setData(bundle);
                             handler.sendMessage(message);
                         }
+//                        //b
+//                        if((thumb>=3.25 && thumb<=3.60) && (index>=3.48 && index<=3.60) && (middle>=3.40 && middle<=3.65) && (ring>=3.45 && ring<=3.65) && (pinky>=3.15 && pinky<=3.40)){
+//                            Message message = Message.obtain();
+////
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "b");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
+//                        //c
+//                        if((thumb>=3.20 && thumb<=3.50) && (index>=2.95 && index<=3.10) && (middle>=3.10 && middle<=3.25) && (ring>=3.25 && ring<=3.45) && (pinky>=3.40 && pinky<=3.60)&&(contact2==2.00)){
+//                            Message message = Message.obtain();
+////
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "c");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
+//                        //d
+//                        if((thumb>=3.30 && thumb<=3.45) && (index>=3.20 && index<=3.36) && (middle>=3.20 && middle<=3.43) && (ring>=3.40 && ring<=3.65) && (pinky>=3.38 && pinky<=3.55) && (contact3==3.00)){
+//                            Message message = Message.obtain();
+//
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "d");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
+//                        //e
+//                        if((thumb>=3.00 && thumb<=3.15) && (index>=2.90 && index<=3.12) && (middle>=2.60 && middle<=2.75) && (ring>=2.95 && ring<=3.15) && (pinky>=2.60 && pinky<=2.90)){
+//                            Message message = Message.obtain();
+////
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "e");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
+//                        //f
+//                        if((thumb>=3.47 && thumb<=3.57) && (index>=3.50 && index<=3.60) && (middle>=3.49 && middle<=3.59) && (ring>=2.96 && ring<=3.16) && (pinky>=3.32 && pinky<=3.50)){
+//                            Message message = Message.obtain();
+////
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "f");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
+//                        //g
+//                        if((thumb>=3.00 && thumb<=3.20) && (index>=2.82 && index<=3.05) && (middle>=2.82 && middle<=3.15) && (ring>=3.60 && ring<=3.80) && (pinky>=3.20 && pinky<=3.51)&&(contact3==3.00)){
+//                            Message message = Message.obtain();
+//
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "g");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
+//                        //h
+//                        if((thumb>=3.00 && thumb<=3.15) && (index>=3.00 && index<=3.20) && (middle>=3.46 && middle<=3.60) && (ring>=3.52 && ring<=3.63) && (pinky>=3.28 && pinky<=3.45)){
+//                            Message message = Message.obtain();
+////
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "h");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
+//                        //i
+//                        if((thumb>=3.20 && thumb<=3.65) && (index>=2.95 && index<=3.15) && (middle>=2.95 && middle<=3.10) && (ring>=2.90 && ring<=3.20) && (pinky>=3.18 && pinky<=3.40)){
+//                            Message message = Message.obtain();
+//
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "i");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
+//                        //j
+//                        if((thumb>=3.40 && thumb<=3.60) && (index>=2.90 && index<=3.10) && (middle>=2.85 && middle<=3.05) && (ring>=2.90 && ring<=3.10) && (pinky>=3.15 && pinky<=3.30)&&(contact6==6.00)){
+//                            Message message = Message.obtain();
+//
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "j");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
+//                        //k
+//                        if((thumb>=2.90 && thumb<=3.10) && (index>=3.02 && index<=3.20) && (middle>=3.46 && middle<=3.60) && (ring>=3.55 && ring<=3.68) && (pinky>=3.41 && pinky<=3.56)){
+//                            Message message = Message.obtain();
+////
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "k");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
+//                        //l
+//                        if((thumb>=2.98 && thumb<=3.12) && (index>=2.90 && index<=3.10) && (middle>=2.80 && middle<=3.05) && (ring>=3.50 && ring<=3.75) && (pinky>=3.35 && pinky<=3.60)){
+//                            Message message = Message.obtain();
+////
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "l");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
+//                        //m
+//                        if((thumb>=2.90 && thumb<=3.15) && (index>=2.90 && index<=3.15) && (middle>=2.90 && middle<=3.15) && (ring>=3.00 && ring<=3.25) && (pinky>=2.90 && pinky<=3.15)){
+//                            Message message = Message.obtain();
+//
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "m");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
+//                        //n
+//                        if((thumb>=2.90 && thumb<=3.15) && (index>=2.95 && index<=3.15) && (middle>=2.90 && middle<=3.15) && (ring>=2.90 && ring<=3.15) && (pinky>=3.00 && pinky<=3.25)&&(contact3==3.00)){
+//                            Message message = Message.obtain();
+////
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "n");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
+//                        //o
+//                        if((thumb>=3.00 && thumb<=3.30) && (index>=3.10 && index<=3.25) && (middle>=3.07 && middle<=3.21) && (ring>=3.10 && ring<=3.30) && (pinky>=3.28 && pinky<=3.42)&&(contact3 ==3.00)){
+//                            Message message = Message.obtain();
+////
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "o");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
+//                        //p
+//                        if((thumb>=2.90 && thumb<=3.05) && (index>=2.89 && index<=3.05) && (middle>=3.31 && middle<=3.50) && (ring>=3.54 && ring<=3.70) && (pinky>=3.29 && pinky<=3.51)) {
+//                            Message message = Message.obtain();
+////
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "p");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
+//                        //q
+//                        if((thumb>=3.40 && thumb<=3.65) && (index>=3.45 && index<=3.65) && (middle>=2.78 && middle<=2.95) && (ring>=3.50 && ring<=3.70) && (pinky>=3.25 && pinky<=3.45)) {
+//                            Message message = Message.obtain();
+////
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "q");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
+//                        //r
+//                        if((thumb>=2.80 && thumb<=3.15) && (index>=2.60 && index<=2.99) && (middle>=3.10 && middle<=3.25) && (ring>=3.45 && ring<=3.70) && (pinky>=3.20 && pinky<=3.45)&&(contact4==4.00)) {
+//                            Message message = Message.obtain();
+////
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "r");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
+//                        //s
+//                        if((thumb>=2.84 && thumb<=2.99) && (index>=2.76 && index<=2.91) && (middle>=2.75 && middle<=2.90) && (ring>=2.76 && ring<=2.91) && (pinky>=3.12 && pinky<=3.27)&&(contact4==4.00)) {
+//                            Message message = Message.obtain();
+////
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "s");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
+//                        //t
+//                        if((thumb>=2.90 && thumb<=3.10) && (index>=2.75 && index<=2.95) && (middle>=2.78 && middle<=2.95) && (ring>=3.15 && ring<=3.30) && (pinky>=3.05 && pinky<=3.23)) {
+//                            Message message = Message.obtain();
+////
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "t");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
+//                        //u
+//                        if((thumb>=3.00 && thumb<=3.32) && (index>=2.75 && index<=2.95) && (middle>=3.50 && middle<=3.65) && (ring>=3.58 && ring<=3.73) && (pinky>=3.30 && pinky<=3.48) && (contact2 == 2.0)) {
+//                            Message message = Message.obtain();
+////
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "u");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
+//                        //v
+//                        if((thumb>=3.10 && thumb<=3.30) && (index>=2.80 && index<=3.10) && (middle>=3.50 && middle<=3.65) && (ring>=3.58 && ring<=3.73) && (pinky>=3.33 && pinky<=3.48)) {
+//                            Message message = Message.obtain();
+//
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "v");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
+//                        //w
+//                        if((thumb>=2.98 && thumb<=3.20) && (index>=3.40 && index<=3.65) && (middle>=2.42 && middle<=3.67) && (ring>=3.50 && ring<=3.75) && (pinky>=3.20 && pinky<=3.50)) {
+//                            Message message = Message.obtain();
+////
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "w");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
+//
+//                        //x
+//                        if((thumb>=2.80 && thumb<=3.13) && (index>=2.75 && index<=3.15) && (middle>=2.70 && middle<=3.05) && (ring>=3.10 && ring<=3.30) && (pinky>=2.75 && pinky<=3.05)) {
+//                            Message message = Message.obtain();
+//
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "x");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
+//                        //y
+//                        if((thumb>=3.47 && thumb<=3.62) && (index>=3.00 && index<=3.13) && (middle>=2.90 && middle<=3.10) && (ring>=2.95 && ring<=3.13) && (pinky>=3.54 && pinky<=3.65)) {
+//                            Message message = Message.obtain();
+//
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "y");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
+//                        //z
+//                        if((thumb>=3.24 && thumb<=3.40) && (index>=3.08 && index<=3.40) && (middle>=3.05 && middle<=3.23) && (ring>=3.50 && ring<=3.73) && (pinky>=3.10 && pinky<=3.25)) {
+//                            Message message = Message.obtain();
+//
+//                            Bundle bundle = new Bundle();
+//                            bundle.putString("key", "z");
+//                            message.setData(bundle);
+//                            handler.sendMessage(message);
+//                        }
 
 
                     }
